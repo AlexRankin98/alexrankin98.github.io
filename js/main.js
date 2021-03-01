3c1de3386c877b4b927a2e8df7c370dadc5d964d
Alex Rankin <71366782+AlexRankin98@users.noreply.github.com>
2021-03-01 23:04:29 +1300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+﻿$(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 400) {
+      $('.showMenu').fadeIn();
+    } else {
+      $('.showMenu').fadeOut();
+    }
+  });
+
+
+  $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 300) {
+      $('.hideMenu').fadeOut();
+    } else {
+      $('.hideMenu').fadeIn();
+    }
+  });
